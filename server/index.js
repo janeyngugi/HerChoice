906f5f6f68ddd00f5cca1175d911445723ce6e74
Murash-ops <charleskiyo5@gmail.com>
2026-02-17 14:12:49 +0300

    Fix routing to client dir and server listen

--- server/index.js
+++ server/index.js
@@ -31,9 +31,11 @@ sequelize.sync().then(async () => {
     console.error("Error during seeding check:", error);
   }
 
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
+  if (require.main === module) {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  }
 }).catch(err => {
   console.error('Unable to connect to the database:', err);
 });
